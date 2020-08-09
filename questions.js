bbd59b2bd5d2f7023e0297d2780160d33c571641
Marium Arif <mariumarif1999@gmail.com>
2020-08-09 23:20:53 +0500

    Assignment 10 Quiz Application

---
+++ questions.js
@@ -0,0 +1,29 @@
+// Array of Objects
+
+const quiz = [
+    {
+        q:'Which is the most populous country in the world?',
+        options:['Turkey','India','Pakistan','China'],
+        answer:3
+    },
+    {
+        q:'How many degrees are there in a right angle?',
+        options:['90','180','30','270'],
+        answer:0
+    },
+    {
+        q:'What is the capital of Australia?',
+        options:['Melbourne','Sydney','Canberra','Adelaide'],
+        answer:2
+    },
+    {
+        q:'Which of these shapes has 13 sides?',
+        options:['Tetrakaidecagon','Triskaidecagon','Enneadecagon','Icosagon'],
+        answer:1
+    },
+    {
+        q:'Subtract the second even number from the third square number',
+        options:['-5','4','5','-4'],
+        answer:2
+    }
+]
